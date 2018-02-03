68c1338dc29de39b0a3ff4e1e0c0e8825ec57b0d
cd10012 <ced361@nyu.edu>
2018-02-02 23:17:35 -0500

    preliminary set up, added linear regression function w test

--- app/index.js
+++ app/index.js
@@ -16,14 +16,48 @@ var ccxt = require('ccxt');
         apiKey: 'YOUR_PUBLIC_API_KEY',
         secret: 'YOUR_SECRET_PRIVATE_KEY',
     })
+    let orderbook = await gdax.fetchOrderBook ('ETH/USD')
+	let bid = orderbook.bids.length ? orderbook.bids[0][0] : undefined
+	let ask = orderbook.asks.length ? orderbook.asks[0][0] : undefined
+	let spread = (bid && ask) ? ask - bid : undefined
+	console.log (gdax.id, 'market price', { bid, ask, spread });
 
-    console.log (gdax.id,    await gdax.loadMarkets ());
-    let updatedMarkets = await gdax.loadMarkets(true);
-    console.log(updatedMarkets);
 
-    console.log (gdax.id,    await gdax.fetchOrderBook (gdax.symbols[0]));
-    console.log (gdax.id,	 await gdax.fetchTicker ('ETH/USD'));
-    console.log (gdax.id,	 await gdax.fetchTrades ('ETH/USD'));
+    //console.log (gdax.id,    await gdax.fetchOrderBook (gdax.symbols[0]));
+    //console.log (gdax.id,	 await gdax.fetchTicker ('ETH/USD'));
+   // console.log (gdax.id,	 await gdax.fetchTrades ('ETH/USD'));
 }) ();
 
+function linearRegression(y,x){
+		var lr = {};
+        var n = y.length;
+        var sum_x = 0;
+        var sum_y = 0;
+        var sum_xy = 0;
+        var sum_xx = 0;
+        var sum_yy = 0;
 
+        for (var i = 0; i < y.length; i++) {
+
+            sum_x += x[i];
+            sum_y += y[i];
+            sum_xy += (x[i]*y[i]);
+            sum_xx += (x[i]*x[i]);
+            sum_yy += (y[i]*y[i]);
+        } 
+
+        lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
+        lr['intercept'] = (sum_y - lr.slope * sum_x)/n;
+        lr['r2'] = Math.pow((n*sum_xy - sum_x*sum_y)/Math.sqrt((n*sum_xx-sum_x*sum_x)*(n*sum_yy-sum_y*sum_y)),2);
+
+        return lr;
+}
+
+var known_y = [1, 2, 3, 4];
+var known_x = [5.2, 5.7, 5.0, 4.2];
+
+var lr = linearRegression(known_y, known_x);
+
+console.log(lr.slope);
+console.log(lr.intercept);
+console.log(lr.r2);
